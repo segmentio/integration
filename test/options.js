9fc612a160560cd61b8d8f2bc4838abd5884e06e
Amir Abushareb <yields@icloud.com>
2014-08-19 16:00:11 +0300

    .option(): fix custom validator

--- test/options.js
+++ test/options.js
@@ -1,4 +1,5 @@
 
+var Track = require('segmentio-facade').Track;
 var integration = require('..');
 var assert = require('assert');
 
@@ -30,5 +31,33 @@ describe('options', function(){
       var err = Segment.validate({}, { token: 'tok' });
       assert.equal(err, null);
     });
+  });
+
+  it('should accept fucntions instead of meta', function(){
+    var Segment = integration('Segment');
+    var conf = {};
+    var message = new Track({});
+    var called;
+
+    Segment.option('token', function(msg, settings){
+      assert.equal(msg, message);
+      assert.equal(settings, conf);
+      called = true;
+      if ('track' != msg.type()) return;
+      if (settings.token) return;
+      return this.error('validation', '.token is required for track calls', {
+        settings: settings,
+        message: msg
+      });
+    });
+
+    var err = Segment.validate(message, conf);
+    assert.equal('Segment: .token is required for track calls', err.message);
+
+    conf.token = 'token';
+    var err = Segment.validate(message, conf);
+    assert.equal(null, err);
+
+    assert(true == called);
   });
 });
