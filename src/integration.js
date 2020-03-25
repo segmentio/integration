bc016c41ce62c85e8cf68c60447cc9a1d40cfd91
Marcus Ericsson <36717+mericsson@users.noreply.github.com>
2020-03-25 07:27:29 -0700

    Tighten flags interface

--- src/integration.js
+++ src/integration.js
@@ -12,6 +12,16 @@ var proto = require('./proto')
 var assert = require('assert')
 var debug = require('debug')
 
+class Flags {
+  constructor(flagArray = []) {
+    this.flagSet = new Set(flagArray)
+  }
+
+  on(flag) {
+    return this.flagSet.has(flag)
+  }
+}
+
 /**
  * Expose `createIntegration`
  */
@@ -23,6 +33,7 @@ exports = module.exports = createIntegration
  */
 
 exports.errors = errors
+exports.Flags = Flags
 
 /**
  * Create integration with `name`.
@@ -44,7 +55,7 @@ function createIntegration (name) {
     if (!(this instanceof Integration)) return new Integration(settings, flags)
     this.debug = debug('segmentio:integration:' + this.slug())
     this.settings = settings || {}
-    this.flags = flags || new Set()
+    this.flags = flags || new Flags()
     Emitter.call(this)
     this.initialize()
     wrapMethods(this)
